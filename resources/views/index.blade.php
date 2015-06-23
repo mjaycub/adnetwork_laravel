08875f30fc69d4992d1748570ed68d0616d990d7
Mark <themarkjacob@gmail.com>
2015-06-23 21:43:46 +0100

    Small Front End Change
    Changed homepage content slightly.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>Hello {{$name}}!</h1>
+	<h1>Bluence Homepage</h1>
 	@if(Session::has('message'))
 		<div>
 			<p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
 		</div>
 	@endif
+	<p>Amazing home page with effective CTA's and amazing copy with the bestest images...</p>
 @stop
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -13,7 +13,7 @@
 	  	<h3>Content Creators</h3>
 	  	<p>Content creators are people who create content across various social media platforms such as YouTube, Vine, Facebook, Twitter, SnapChat and more! Register with Bluence for the opportunity to receive 
 	  		advertising deals from companies around the world to help fund your content.</p>
-	  		<a class="btn btn-primary btn-lg btn-block" href="/users/create" role="button">Content Creators</a>
+	  		<a class="btn btn-primary btn-lg btn-block" href="/creators/create" role="button">Content Creators</a>
 	  </div>
 
 	  <div class="col-md-6">
